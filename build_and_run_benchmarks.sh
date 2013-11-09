a9ea90ffa2eb5908e26bcc22f98454e8d0088ec0
Ryan Newton <rrnewton@gmail.com>
2013-11-08 20:42:46 -0500

    Get the benchmarking into a reasonable initial state.  Required a tweak to hsbencher.

---
+++ build_and_run_benchmarks.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+set -x
+set -e
+
+# If given an argument, the first is a directory:
+if [ "$1" != "" ]; then 
+  cd $1
+  shift
+fi
+
+if ! [ -f dpjbin.tar.gz ]; then
+  wget http://dpj.cs.uiuc.edu/DPJ/Download_files/dpjbin.tar.gz
+fi 
+if ! [ -f ./dpjbin/Implementation/bin/dpj ]; then
+  tar xzvf dpjbin.tar.gz
+fi
+
+source envvars.sh
+
+echo "Patching the Harness.java file and Makefile..."
+cp -v Harness.java dpjbin/Benchmarks/Kernels/dpj/Harness.java
+cp -v tweaked_Makefile dpjbin/Benchmarks/Kernels/Makefile
+
+# cabal sandbox init
+# cabal install --only-dependencies -j 
+# cabal build
+cabal install -j ./ ./HSBencher/
+
+# Simple way:
+# ./run_benchmark --trials=5 --server
+
+# Complicated way to run the benchmarks:
+# ----------------------------------------
+# export TRIALS=5
+export TRIALS=1
+
+# Parfunc account, registered app in api console:
+CID=905767673358.apps.googleusercontent.com
+SEC=2a2H57dBggubW1_rqglC7jtK
+
+# LVish doc ID:  
+TABID=1YxEmNpeUoGCBptDK0ddtomC_oK2IVH1f2M89IIA
+# https://www.google.com/fusiontables/DataSource?docid=1YxEmNpeUoGCBptDK0ddtomC_oK2IVH1f2M89IIA#rows
+
+# Enable upload of benchmarking data to a Google Fusion Table:
+# ./run_benchmark --server --keepgoing --trials=$TRIALS --fusion-upload=$TABID --clientid=$CID --clientsecret=$SEC $*
+./run_benchmark --server --keepgoing --trials=$TRIALS --fusion-upload=$TABID --clientid=$CID --clientsecret=$SEC $*
---
+++ envvars.sh
@@ -0,0 +1,5 @@
+# source this
+
+# export DPJ_ROOT=`pwd`/dpjbin/build/dpjbin
+export DPJ_ROOT=`pwd`/dpjbin/
+export PATH=$PATH:$DPJ_ROOT/Implementation/bin
